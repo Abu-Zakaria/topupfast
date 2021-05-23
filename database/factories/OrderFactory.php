5fabb4c7bcb6454c114606a867c2606f629ac8f6
Jakaria Blaine <jakariablaine120@gmail.com>
2021-05-24 04:30:35 +0600

    Add order seed and factories

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -4,9 +4,19 @@
 
 use App\Order;
 use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Hash;
 
 $factory->define(Order::class, function (Faker $faker) {
     return [
-        //
+        'user_id' => 4,
+        'package_id' => 1,
+        'package_name' => $faker->colorName(),
+        'buy_price' => $faker->numberBetween(10, 100),
+        'sale_price' => $faker->numberBetween(10, 100),
+        'playerid' => $faker->numberBetween(4, 10),
+        'password' => "12345678",
+        'accounttype' => 'normal',
+        'securitycode' => '879',
+        'status' => 'pending',
     ];
 });
