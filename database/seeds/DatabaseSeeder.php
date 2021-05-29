90ee382d9a770f0eab84370063e3bed72343a7c5
Jakaria Blaine <jakariablaine120@gmail.com>
2021-05-30 00:51:56 +0600

    Remove permission seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     {
          $this->call([
            UsersTableSeeder::class,
-           PermissionSeeder::class,
-           PermissionRoleSeeder::class,
+           // PermissionSeeder::class,
+           // PermissionRoleSeeder::class,
            BloodGroupSeeder::class,
            GenderSeeder::class,
            ReligionSeeder::class,
