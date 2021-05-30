16f5c998ca8f190bce1e3c1f237cca0cbf1b2986
Jakaria Blaine <jakariablaine120@gmail.com>
2021-05-30 21:18:17 +0600

    Detect active status

--- app/Http/Middleware/IsSeller.php
+++ app/Http/Middleware/IsSeller.php
@@ -15,7 +15,7 @@ class IsSeller
      */
     public function handle($request, Closure $next)
     {
-        if(auth()->user()->is_admin == 2)
+        if(auth()->user()->is_admin == 2 && auth()->user()->status == 1)
         {
             return $next($request);
         }
